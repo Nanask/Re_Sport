0dfb37197dfd1136eb7d53e785e2b25e4d21bea2
Nanask <ybhag1005@daum.net>
2022-02-08 14:15:42 +0900

    first commit

---
+++ src/main/java/com/nana/sport/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.nana.sport.config;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/**")
+                .anonymous().mvcMatchers("/admin").fullyAuthenticated();
+
+
+
+
+
+    }
+}
+
